4b6b430615ed8d96c26f4645a1a5327ad06e9f5f
Ilya Kuznetsov <ilyakuznecov84@gmail.com>
2026-09-22 23:33:53 +0300

    Added authentication flow

--- registry/src/main/java/com/euphoriav/docker/registry/controller/AuthenticationExceptionHandler.java
+++ registry/src/main/java/com/euphoriav/docker/registry/controller/AuthenticationExceptionHandler.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.AuthenticationException;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AuthenticationExceptionHandler implements AuthenticationEntryPoint {
@@ -30,7 +28,6 @@ public class AuthenticationExceptionHandler implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        log.error("authentication exception %s %s".formatted(request.getMethod(), request.getRequestURL()), authException);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setHeader(HttpHeaders.WWW_AUTHENTICATE, String.format("Bearer realm=\"%s\",service=\"%s\"", realm, appName));
         response.setContentType("application/json");
